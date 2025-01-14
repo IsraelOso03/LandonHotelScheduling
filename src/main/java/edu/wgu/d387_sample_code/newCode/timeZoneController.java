9ef35f87d74ae0ec9be3bce69a003523a8f9ec39
iosori6 <iosori6@wgu.edu>
2025-01-13 21:51:38 -0800

    Added 1 hour to the current time for the time conversions.

--- src/main/java/edu/wgu/d387_sample_code/newCode/timeZoneController.java
+++ src/main/java/edu/wgu/d387_sample_code/newCode/timeZoneController.java
@@ -4,6 +4,7 @@ import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.time.LocalDate;
 import java.time.LocalDateTime;
 import java.util.Map;
 
@@ -15,7 +16,7 @@ public class timeZoneController {
     @CrossOrigin(origins = "http://localhost:4200")
     @GetMapping("/presentation-times")
     public Map<String, String> getPresentationTimes() {
-        LocalDateTime presentationTime = LocalDateTime.of(2025, 1, 15, 14, 0); // 2:00 PM ET
+        LocalDateTime presentationTime = LocalDateTime.now().plusHours(1);
         return timeZoneService.getPresentationTimes(presentationTime);
     }
 }
